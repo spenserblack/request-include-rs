d9d3c6acd8b5dbdae3f07829c23a051b5dcada16
Spenser Black <spenserblack01@gmail.com>
2020-02-20 11:09:27 -0500

    GET Response as const

--- curl_include/examples/basic.rs
+++ curl_include/examples/basic.rs
@@ -1,6 +1,6 @@
 use curl_include::include_str as curl_str;
 
 fn main() {
-    let val = curl_str!("https://api.github.com/search/repositories?q=tetris&sort=stars&order=desc");
-    println!("{:?}", val);
+    const VAL: &str = curl_str!("https://api.github.com/search/repositories?q=tetris&sort=stars&order=desc");
+    println!("{:?}", VAL);
 }
--- curl_include_impl/src/lib.rs
+++ curl_include_impl/src/lib.rs
@@ -5,12 +5,17 @@ use proc_macro_hack::proc_macro_hack;
 use quote::quote;
 use syn::parse_macro_input;
 
+const HARD_CODED: &str = "https://api.github.com/search/repositories?q=tetris&sort=stars&order=desc";
+
 #[proc_macro_hack]
 pub fn include_str(input: TokenStream) -> TokenStream {
-    // unimplemented!("Get value");
-    let val = "test";
+    let body = reqwest::blocking::get(HARD_CODED)
+        .unwrap()
+        .text()
+        .unwrap();
+    let s = body.as_str();
     let token_stream = quote! {
-        #val
+        #s
     };
     TokenStream::from(token_stream)
 }
