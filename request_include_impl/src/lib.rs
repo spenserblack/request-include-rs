bb49f4d68f02b30715e7fc8e0ff042760805198b
Spenser Black <spenserblack01@gmail.com>
2020-08-05 13:08:24 -0400

    Parse token input

--- request_include_impl/src/lib.rs
+++ request_include_impl/src/lib.rs
@@ -1,15 +1,27 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
+use proc_macro::{TokenStream, TokenTree};
 use proc_macro_hack::proc_macro_hack;
 use quote::quote;
 use syn::parse_macro_input;
 
-const HARD_CODED: &str = "https://api.github.com/search/repositories?q=tetris&sort=stars&order=desc";
+const ERROR: &str = "Invalid input";
 
 #[proc_macro_hack]
 pub fn include_str(input: TokenStream) -> TokenStream {
-    let body = reqwest::blocking::get(HARD_CODED)
+    let input: String = match input.into_iter().next() {
+        Some(TokenTree::Literal(literal)) => {
+            let literal = literal.to_string();
+            let mut chars = literal.chars();
+            match (chars.next(), chars.next_back()) {
+                (Some('"'), Some('"')) => {},
+                _ => panic!(ERROR),
+            }
+            chars.collect()
+        },
+        _ => panic!(ERROR),
+    };
+    let body = reqwest::blocking::get(&input)
         .unwrap()
         .text()
         .unwrap();
