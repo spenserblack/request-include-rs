bcd58ec74bef9bec6b6356d36ea4520a6fcc88d7
Spenser Black <spenserblack01@gmail.com>
2020-08-06 16:53:39 -0400

    Accept user agent

--- request_include_impl/src/lib.rs
+++ request_include_impl/src/lib.rs
@@ -9,7 +9,8 @@ const ERROR: &str = "Invalid input";
 
 #[proc_macro_hack]
 pub fn include_str(input: TokenStream) -> TokenStream {
-    let input: String = match input.into_iter().next() {
+    let mut tokens = input.into_iter();
+    let input: String = match tokens.next() {
         Some(TokenTree::Literal(literal)) => {
             let literal = literal.to_string();
             let mut chars = literal.chars();
@@ -21,13 +22,55 @@ pub fn include_str(input: TokenStream) -> TokenStream {
         },
         _ => panic!(ERROR),
     };
-    let body = reqwest::blocking::get(&input)
-        .expect("Failed to get response")
-        .text()
-        .expect("Failed to parse response");
-    let s = body.as_str();
-    let token_stream = quote! {
-        #s
+    let user_agent: Option<String> = match tokens.next() {
+        Some(TokenTree::Punct(punct)) => {
+            let punct = punct.as_char();
+            if punct != ',' {
+                panic!("Unexpected token: {:?}", punct);
+            }
+            match tokens.next() {
+                Some(TokenTree::Literal(literal)) => {
+                    let literal = literal.to_string();
+                    let mut chars = literal.chars();
+                    match (chars.next(), chars.next_back()) {
+                        (Some('"'), Some('"')) => {},
+                        _ => panic!(ERROR),
+                    }
+                    Some(chars.collect())
+                },
+                _ => panic!(ERROR),
+            }
+        },
+        None => None,
+        _ => panic!(ERROR),
     };
-    TokenStream::from(token_stream)
+    match user_agent {
+        None => {
+            let body = reqwest::blocking::get(&input)
+                .expect("Failed to get response")
+                .text()
+                .expect("Failed to parse response");
+            let s = body.as_str();
+            let token_stream = quote! {
+                #s
+            };
+            TokenStream::from(token_stream)
+        },
+        Some(user_agent) => {
+            let body = reqwest::blocking::Client::builder()
+                .user_agent(&user_agent)
+                .build()
+                .expect("Failed to build client")
+                .get(&input)
+                .send()
+                .expect("Failed to get response")
+                .text()
+                .expect("Failed to parse response to text");
+            let s = body.as_str();
+            let token_stream = quote! {
+                #s
+            };
+            TokenStream::from(token_stream)
+        },
+    }
 }
