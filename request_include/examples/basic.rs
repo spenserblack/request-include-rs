6f2d0965d192db747076f9c93f576caeb1b38fb1
Spenser Black <spenserblack01@gmail.com>
2020-08-20 12:18:40 -0400

    Change println to comments

--- request_include/examples/basic.rs
+++ request_include/examples/basic.rs
@@ -1,7 +1,8 @@
 use request_include::include_str as request_str;
 
 fn main() {
-    println!(r#"We're using "request-include example" as our user agent, but you should define your own in your project"#);
+    /// We're using `"request-include example"` as our user agent, but you
+    /// should define your own in your project.
     const VAL: &str = request_str!("https://api.github.com/search/repositories?q=tetris&sort=stars&order=desc", "request-include example");
     println!("{:?}", VAL);
 }
