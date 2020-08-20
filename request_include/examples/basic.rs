62ec1de0b73258b17d0a711b093055edc53637af
Spenser Black <spenserblack01@gmail.com>
2020-08-20 11:53:37 -0400

    Change how example represents user agent

--- request_include/examples/basic.rs
+++ request_include/examples/basic.rs
@@ -1,6 +1,7 @@
 use request_include::include_str as request_str;
 
 fn main() {
-    const VAL: &str = request_str!("https://api.github.com/search/repositories?q=tetris&sort=stars&order=desc", "my-user-agent (<contact info>)");
+    println!(r#"We're using "request-include example" as our user agent, but you should define your own in your project"#);
+    const VAL: &str = request_str!("https://api.github.com/search/repositories?q=tetris&sort=stars&order=desc", "request-include example");
     println!("{:?}", VAL);
 }
